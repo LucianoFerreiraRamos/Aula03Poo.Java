aae2f615aa5c63b0e5c87b3beaffbd0b7b14f34b
Luciano <97047885+LucianoFerreiraRamos@users.noreply.github.com>
2022-02-17 14:54:51 -0300

    Aula03Poo

---
+++ src/aula03poo/Aula03Poo.java
@@ -0,0 +1,18 @@
+package aula03poo;
+
+public class Aula03Poo {
+
+	public static void main(String[] args) {
+		Caneta c1 = new Caneta();
+		c1.modelo="big cristal";
+		c1.cor = "azul";
+		//c1.ponta = 0.5f;
+		c1.carga = 80;
+		//c1.tampada = false;
+		c1.tampar();
+		c1.status();
+		c1.rabiscar();
+
+	}
+
+}
---
+++ src/aula03poo/Caneta.java
@@ -0,0 +1,42 @@
+package aula03poo;
+
+public class Caneta {
+	
+	
+	public String modelo;
+	public String cor;
+	private float ponta;
+	protected int carga;
+	private boolean tampada;
+	public void status() {
+		System.out.println("Modelo: " + this.modelo);
+		System.out.println("Uma caneta: " + this.cor);
+		System.out.println("Ponta: " + this.ponta);
+		System.out.println("Carga: " + this.carga);
+		System.out.println("Está tampada ? " + this.tampada);
+	}
+	
+	public void rabiscar() {
+		if (this.tampada == true) {
+			System.out.println("Erro ! Nao posso rabiscar " );
+			
+		}else {
+			
+			System.out.println("Estou rabiscando ");
+		}
+		
+	}
+	
+	public void tampar() {
+		this.tampada = true;
+	}
+	
+	public void destampar () {
+		this.tampada = false;
+	}	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
